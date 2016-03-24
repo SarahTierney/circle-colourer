21abb1af701b93da4d64529decc0ef93f4723744
SarahTierney <tierney.s@live.com>
2016-03-24 17:48:17 -0400

    finished js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ var $html = $('html');
 var $ball = $('ball');
 var $picker = $('form');
 var $input = $('#colour');
+
+$picker.on('change', function() {
+  console.log('works');
+  $ball.css('background-color', $input.val());
+});
